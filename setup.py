af8c1402235340cb3e14e68ebba6d432414838bc
Jeff Mo <jeff.mo@xero.com>
2019-04-16 17:30:22 +1200

    Bump version

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='laziter',
     url='github.com/jeffmomo/laziter',
-    version='0.0.1',
+    version='0.0.2',
     py_modules=['laziter'],
 )
