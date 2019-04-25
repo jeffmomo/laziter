827b0b0ab9485ed30e1c410e1130003e9a8866da
Jeff Mo <jeff.mo@xero.com>
2019-04-25 14:09:13 +1200

    Finally the pacakge works

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
+from pathlib import Path
 
 setup(
     name='laziter',
-    url='github.com/jeffmomo/laziter',
-    version='0.0.4',
+    url='https://github.com/jeffmomo/laziter',
+    author='Jeff Mo',
+    description='A lazy iterator wrapper with multiprocessing powers!',
+    long_description=(Path(__file__).parent/'README.md').open('r').read(),
+    version='0.0.5',
     py_modules=['laziter'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
