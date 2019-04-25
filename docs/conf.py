11fc4d311e820373fc95d64d1564938d754ec0fc
Jeff Mo <jeff.mo@xero.com>
2019-04-25 21:13:17 +1200

    make it work hopefully

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent))
 from laziter import laziter
 
 # -- Project information -----------------------------------------------------
